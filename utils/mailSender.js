76b4f58f4584f83f547f3508e31a21a11fb4e76d
gaurav <gauravgs.1290@gmail.com>
2026-04-03 10:59:16 +0530

    adding brevo code

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,27 +1,66 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+
+// exports.mailSender = async (to, subject, html) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: process.env.MAIL_HOST,
+//       port: process.env.MAIL_PORT,
+//       secure: false, 
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     const info = await transporter.sendMail({
+//       from: `"FILO Support" <${process.env.MAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     });
+
+//     return info;
+//   } catch (error) {
+//     console.log(" sendEmail error:", error.message);
+//     throw error;
+//   }
+// };
+
+
+
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 exports.mailSender = async (to, subject, html) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
-      secure: false, 
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+    const client = SibApiV3Sdk.ApiClient.instance;
+
+    const apiKey = client.authentications["api-key"];
+    apiKey.apiKey = process.env.BREVO_API_KEY;
+
+    const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    const sender = {
+      email: process.env.BREVO_EMAIL, 
+      name: "FILO Support",
+    };
+
+    const receivers = [
+      {
+        email: to,
       },
+    ];
+
+    const response = await tranEmailApi.sendTransacEmail({
+      sender,
+      to: receivers,
+      subject: subject,
+      htmlContent: html,
     });
 
-    const info = await transporter.sendMail({
-      from: `"FILO Support" <${process.env.MAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
+    return response;
 
-    return info;
   } catch (error) {
-    console.log(" sendEmail error:", error.message);
+    console.log("sendEmail error:", error.message);
     throw error;
   }
 };
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,53 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-exports.sendEmail = async (to,subject,html) => {
-    const transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        }
-    })
-    await transporter.sendMail({
-        from:`"FILO" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html,
-    })
-}
+// exports.sendEmail = async (to,subject,html) => {
+//     const transporter = nodemailer.createTransport({
+//         service:"gmail",
+//         auth:{
+//             user:process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASS,
+//         }
+//     })
+//     await transporter.sendMail({
+//         from:`"FILO" <${process.env.EMAIL_USER}>`,
+//         to,
+//         subject,
+//         html,
+//     })
+// }
 
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
+exports.sendEmail = async (to, subject, html) => {
+    try {
+        const client = SibApiV3Sdk.ApiClient.instance;
+
+        const apiKey = client.authentications["api-key"];
+        apiKey.apiKey = process.env.BREVO_API_KEY;
+
+        const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+        const sender = {
+            email: process.env.BREVO_EMAIL,
+            name: "FILO",
+        };
+
+        const receivers = [
+            {
+                email: to,
+            },
+        ];
+
+        await tranEmailApi.sendTransacEmail({
+            sender,
+            to: receivers,
+            subject: subject,
+            htmlContent: html,
+        });
+
+    } catch (error) {
+        console.error("Brevo Email Error:", error);
+        throw error;
+    }
+};
 
