b267530670b9091246decd64b08daba61a37f48b
gaurav <gauravgs.1290@gmail.com>
2026-04-03 14:43:59 +0530

    final changes

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,33 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// exports.mailSender = async (to, subject, html) => {
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       host: process.env.MAIL_HOST,
-//       port: process.env.MAIL_PORT,
-//       secure: false, 
-//       auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS,
-//       },
-//     });
-
-//     const info = await transporter.sendMail({
-//       from: `"FILO Support" <${process.env.MAIL_USER}>`,
-//       to,
-//       subject,
-//       html,
-//     });
-
-//     return info;
-//   } catch (error) {
-//     console.log(" sendEmail error:", error.message);
-//     throw error;
-//   }
-// };
-
-
-
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 exports.mailSender = async (to, subject, html) => {
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,21 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// exports.sendEmail = async (to,subject,html) => {
-//     const transporter = nodemailer.createTransport({
-//         service:"gmail",
-//         auth:{
-//             user:process.env.EMAIL_USER,
-//             pass: process.env.EMAIL_PASS,
-//         }
-//     })
-//     await transporter.sendMail({
-//         from:`"FILO" <${process.env.EMAIL_USER}>`,
-//         to,
-//         subject,
-//         html,
-//     })
-// }
-
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 exports.sendEmail = async (to, subject, html) => {
